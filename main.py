f8cdb52fa825530b3fba03f9568e6365b40ac678
yunus <notd0535@gmail.com>
2025-04-03 10:05:33 +0800

    version;

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from database.XYBotDB import XYBotDB
 class FeiShuWiki(PluginBase):
     description = "获取飞书Wiki多维表内容"
     author = "yunus"
-    version = "1.0.0"
+    version = "1.0.1"
 
     # 同步初始化
     def __init__(self):
